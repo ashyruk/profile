c7c09e87e77fb54d6ec3ca97addc5da22342feb3
ashyruk <valerarybakbot@gmail.com>
2017-05-05 16:09:19 +0300

    skills

--- js/view.js
+++ js/view.js
@@ -1,3 +1,13 @@
 /**
  * Created by rybak on 18.01.2017.
  */
+window.addEventListener('scroll', function(){
+    console.log(this.scrollY);
+    var elem = document.getElementsByTagName('header')[0];
+    if (this.scrollY>600){
+        elem.className = 'nav fixed';
+    }else{
+        elem.className = 'nav';
+    }
+});
+//TODO:some text here what need todo
